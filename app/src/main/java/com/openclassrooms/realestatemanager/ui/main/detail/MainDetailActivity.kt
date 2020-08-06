96691e65a0e466d742a9a7e2b6c71dff290f5030
Paul Leclerc <paulleclerc@icloud.com>
2020-08-06 17:00:36 +0200

    Made conversion for detail in phone

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/main/detail/MainDetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/main/detail/MainDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.openclassrooms.realestatemanager.R
@@ -19,19 +20,22 @@ import com.openclassrooms.realestatemanager.ui.main.detail.EstateDetailFragment.
  */
 class MainDetailActivity : AppCompatActivity(), ConvertibleActivity {
     override val context: Context = this
-    override var preferences: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+    override lateinit var preferences: SharedPreferences
+    private lateinit var fragment: EstateDetailFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_detail)
+
+        preferences = getPreferences(Context.MODE_PRIVATE)
 
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
             val arguments = Bundle()
             arguments.putSerializable(ARG_ESTATE,
-            intent.getSerializableExtra(ARG_ESTATE))
-            val fragment = EstateDetailFragment(isDollar)
+                    intent.getSerializableExtra(ARG_ESTATE))
+            fragment = EstateDetailFragment(isDollar)
             fragment.arguments = arguments
             supportFragmentManager.beginTransaction()
                     .add(R.id.item_detail_container, fragment)
@@ -39,19 +43,31 @@ class MainDetailActivity : AppCompatActivity(), ConvertibleActivity {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home) {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.convert_menu, menu)
+        menu?.getItem(0)?.setIcon(if (isDollar) R.drawable.euro else R.drawable.dollar)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            navigateUpTo(Intent(this, MainActivity::class.java))
-            return true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                navigateUpTo(Intent(this, MainActivity::class.java))
+                return true
+            }
+            R.id.menu_convert -> didTapConvert(item)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun didTapConvert(item: MenuItem) {
+        super.didTapConvert(item)
+        fragment.convert(isDollar)
     }
 }
