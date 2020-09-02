d328baf4d0adac813e0303a896bbd757a0eb166a
Paul Leclerc <paulleclerc@icloud.com>
2020-09-02 15:25:49 +0200

    Tested ContentProvider

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/ContentProviderTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/ContentProviderTest.kt
@@ -5,7 +5,6 @@ import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
-import android.net.Uri
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.platform.app.InstrumentationRegistry
 import com.openclassrooms.realestatemanager.provider.EstateContentProvider
@@ -13,6 +12,7 @@ import junit.framework.TestCase.assertEquals
 import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.Before
 import org.junit.Test
+import kotlin.random.Random
 
 
 class ItemContentProviderTest {
@@ -29,23 +29,24 @@ class ItemContentProviderTest {
     @Test
     fun insertAndGetEstate() {
         // BEFORE : Adding demo item
-        val userUri: Uri? = contentResolver!!.insert(EstateContentProvider.URI_ITEM, generateItem())
+        val price = Random.nextInt()
+        contentResolver!!.insert(EstateContentProvider.URI_ITEM, generateEstate(price))
         // TEST
         val cursor: Cursor? = contentResolver?.query(ContentUris.withAppendedId(EstateContentProvider.URI_ITEM, USER_ID), null, null, null, null)
         assertThat(cursor, notNullValue())
         assertEquals(cursor?.moveToLast(), true)
-        assertEquals(cursor?.getString(cursor.getColumnIndexOrThrow("description")), "Petit appartement dans Paris")
+        assertEquals(cursor?.getInt(cursor.getColumnIndexOrThrow("price")), price)
     }
 
     // ---
-    private fun generateItem(): ContentValues? = ContentValues().apply {
+    private fun generateEstate(price: Int): ContentValues? = ContentValues().apply {
         put("addressNumber", 23)
         put("addressRoute", "rue de la république")
         put("addressCity", "Paris")
         put("addressPostCode", 75000)
         put("addressCountry", "France")
         put("estateType", "HOUSE")
-        put("estatePrice", 374926)
+        put("estatePrice", price)
         put("estateRooms", 4)
         put("estateArea", 54)
         put("estateDescription", "Petit appartement dans Paris")
